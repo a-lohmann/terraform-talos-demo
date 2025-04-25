2f14fe03e9f1c5b1f97f20f5ec565f8ca32014a1
Antal Lohmann <antal.lohmann@xantara-it.nl>
2025-04-25 15:44:02 +0200

    Init argo config

--- terraform/modules/bootstrap_argocd/main.tf
+++ terraform/modules/bootstrap_argocd/main.tf
@@ -8,3 +8,8 @@ resource "helm_release" "argocd" {
   timeout          = 600
   values           = [file("${path.module}/argocd.yaml")]
 }
+
+resource "kubernetes_manifest" "argocd_apps" {
+  manifest = yamldecode(file("${path.module}/../../../deployments/argocd/argocd-apps.yaml"))
+  depends_on = [ helm_release.argocd ]
+}
